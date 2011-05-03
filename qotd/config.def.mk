89787646a6eb10bc5e252148646580feccb3c195
Gabriel Laskar <gabriel@lse.epita.fr>
2011-05-03 15:11:27 +0200

    Small changes to build system
    * added -D_POSIX_C_SOURCE to CPPFLAGS
    * suppressed config.h and config.def.h

--- qotd/config.def.mk
+++ qotd/config.def.mk
@@ -9,12 +9,12 @@ MANPREFIX = ${PREFIX}/share/man
 
 # includes and libs
 INCS = -I. -I/usr/include
-LIBS = -L/usr/lib -lc 
+LIBS = -L/usr/lib -lc
 
 # flags
-CPPFLAGS = -DVERSION=\"${VERSION}\"
-CFLAGS += -std=c99 -pedantic -Wall ${INCS} ${CPPFLAGS}
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=2
+CFLAGS += -std=c99 -pedantic -Wall -Wextra ${INCS} ${CPPFLAGS}
 LDFLAGS += -s ${LIBS}
 
 # compiler and linker
-CC ?= clang
+CC ?= cc
